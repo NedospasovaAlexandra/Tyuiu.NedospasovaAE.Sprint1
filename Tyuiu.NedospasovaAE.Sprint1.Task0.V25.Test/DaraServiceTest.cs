8fa5d842637e0ea41f6387be3cec077291428942
Tyuiu.NedospasovaAE@outlook.com <anedospasova34@gmail.com>
2024-05-08 10:08:20 +0500

    Add project files.

---
+++ Tyuiu.NedospasovaAE.Sprint1.Task0.V25.Test/DaraServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NedospasovaAE.Sprint1.Task0.V25.Lib;
+
+namespace Tyuiu.NedospasovaAE.Sprint1.Task0.V25.Test
+{
+    [TestClass]
+    public class DaraServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(3, res);
+        }
+    }
+}
---
+++ Tyuiu.NedospasovaAE.Sprint1.Task0.V25.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NedospasovaAE.Sprint1.Task0.V25.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NedospasovaAE.Sprint1.Task0.V25.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0210c19e-142f-47f3-bd59-372a83775bac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
