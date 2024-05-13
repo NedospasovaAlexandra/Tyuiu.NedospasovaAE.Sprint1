a95779c96a3b57e9e17176f62a25683d46af8b5b
Tyuiu.NedospasovaAE@outlook.com <anedospasova34@gmail.com>
2024-05-13 21:06:36 +0500

    Task6

---
+++ Tyuiu.NedospasovaAE.Sprint1.Task6.V3.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NedospasovaAE.Sprint1.Task6.V3.Lib;
+
+namespace Tyuiu.NedospasovaAE.Sprint1.Task6.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidString()
+        {
+            string strTest = "Привет как дела";
+            DataService ds = new DataService();
+            string res = ds.LastLetterWord(strTest);
+            string wait = "тка";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.NedospasovaAE.Sprint1.Task6.V3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.NedospasovaAE.Sprint1.Task6.V3.Lib;
+
+namespace Tyuiu.NedospasovaAE.Sprint1.Task6.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Недоспасова.А.Э | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Работа со строками класс String                                   *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнила: Недоспасова Александра Эдуардовна | ИИПб-23-2                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ: Напечатать строку, составленную из последних литер всех слов,  *");
+            Console.WriteLine("* написанных пользователем                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Напишите текст                                                             ");
+            string resultat = Console.ReadLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.LastLetterWord(resultat);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
