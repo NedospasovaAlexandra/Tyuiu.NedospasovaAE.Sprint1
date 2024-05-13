e578e069ca5255d9d9ea4b17571f2b97f25d458b
Tyuiu.NedospasovaAE@outlook.com <anedospasova34@gmail.com>
2024-05-13 18:16:14 +0500

    Task3

---
+++ Tyuiu.NedospasovaAE.Sprint1.Task3.V16.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NedospasovaAE.Sprint1.Task3.V16.Lib;
+
+namespace Tyuiu.NedospasovaAE.Sprint1.Task3.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x1 = -1;
+            double x2 = -8;
+            double finish = -x1 - x2;
+            var res = ds.CoeffOfQuadraticEquation(x1, x2);
+            Assert.AreEqual(finish, res);
+            
+        }
+    }
+}
---
+++ Tyuiu.NedospasovaAE.Sprint1.Task3.V16/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.NedospasovaAE.Sprint1.Task3.V16.Lib;
+
+namespace Tyuiu.NedospasovaAE.Sprint1.Task3.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            double x1, x2;
+            Console.Title = "Спринт #1 | Выполнила: Недоспасова.А.Э | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнила: Недоспасова Александра Эдуардовна | ИИПб-23-2                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ: Найти коэффициент приведенного квадратного уравнения           *");
+            Console.WriteLine("* по заданным пользователем корням уравнения                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите первый корень:                                                   ");
+            x1= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второй корень:                                                   ");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.CoeffOfQuadraticEquation(x1, x2));
+            Console.ReadKey();
+
+        }
+    }
+}
